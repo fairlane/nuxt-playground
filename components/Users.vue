56bc13ff6741d14ad226000c401e61b2e519a1e9
Hannu <>
2022-01-28 17:15:14 +0200

    Initial commit for nuxt playground

---
+++ components/Users.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="card">
+  <div class="card-header bg-info">
+    User list
+  </div>
+  <div class="card-body">
+    <ul v-for="user in users" :key="user.id">
+      <li>User #{{user.id}} {{user.username}}</li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<script>
+import { getRuntimeConfig } from '../config/settings'
+export default {
+  name: 'Users',
+  data() {
+    return {
+      users: []
+    }
+  },
+  async mounted() {
+    let response = await fetch(getRuntimeConfig().apiUrl + '/list-users');
+    this.users = await response.json();
+    
+    console.log(`I'm mounted and fetched `);
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <users/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Users from '~/components/Users.vue';
+
+export default Vue.extend({
+  components: { Users },
+  name: 'IndexPage'
+})
+</script>
